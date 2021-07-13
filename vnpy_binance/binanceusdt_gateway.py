06b2ef132ca7318ac1b99653853d03a2a38a9a85
noranhe <63988546+noranhe@users.noreply.github.com>
2021-07-13 10:49:17 +0800

    回滚之前关于resp没有text报错的修改

--- vnpy_binance/binanceusdt_gateway.py
+++ vnpy_binance/binanceusdt_gateway.py
@@ -692,12 +692,7 @@ class BinanceUsdtRestApi(RestClient):
 
             # 如果请求失败则终止循环
             if resp.status_code // 100 != 2:
-                data: dict = resp.json()
-                if data["msg"]:
-                    m = data["msg"]
-                else:
-                    m = "无"
-                msg: str = f"获取历史数据失败，状态码：{resp.status_code}，信息：{m}"
+                msg: str = f"获取历史数据失败，状态码：{resp.status_code}，信息：{resp.text}"
                 self.gateway.write_log(msg)
                 break
             else:
